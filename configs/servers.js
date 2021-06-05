98589e5429c5e386683bb3ca962e497a4c8a07df
advuianime <68636322+advuianime@users.noreply.github.com>
2021-06-06 00:07:23 +0700

    Update servers.js

--- configs/servers.js
+++ configs/servers.js
@@ -1,3 +1,3 @@
 "use strict";
 exports.__esModule = true;
-exports.ALLOW_DOMAIN = ["cki.name","cbey.io","javchina.cc","www.jwplayer.com"]; //Domain you want to play. Put [] if you want to play on all domains.
+exports.ALLOW_DOMAIN = ["embed.cki.name","cki.name","cbey.io","javchina.cc","www.jwplayer.com"]; //Domain you want to play. Put [] if you want to play on all domains.
